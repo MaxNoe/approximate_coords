baf45b0ff58bbcc881e31bee682dccb56a806373
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2020-08-13 15:10:55 +0200

    Add performance measurement

--- benchmarks/only_precession.py
+++ benchmarks/only_precession.py
@@ -4,23 +4,37 @@ import astropy.units as u
 import numpy as np
 
 from approximate_coords.almanac_chp_6 import approximate_precesssion
+from approximate_coords.altaz_cirs import no_polar_motion
 import warnings
 from astropy.utils.exceptions import ErfaWarning, AstropyWarning
 import matplotlib.pyplot as plt
+from time import perf_counter
 
 warnings.filterwarnings('ignore', category=ErfaWarning)
 warnings.filterwarnings('ignore', category=AstropyWarning)
 
 
 coord = SkyCoord(ra='90d', dec='45d')
-time = Time('2000-01-01T00:01') + u.Quantity(np.arange(0, 50 * 365, 7), u.day)
+time = Time('2000-01-01T00:01') + u.Quantity(np.arange(0, 50 * 365, 0.1), u.day)
 location = EarthLocation.of_site('Roque de los Muchachos')
+print(f'Transforming coordinates for {len(time)} timestamps')
 
 coord_altaz = coord.transform_to(AltAz(obstime=time, location=location))
 
 
+t0 = perf_counter()
+coord_icrs = coord_altaz.transform_to(ICRS)
+astropy_time = perf_counter() - t0
+print(f'Normal astropy transform took: {astropy_time:.4} s')
+
+
 with approximate_precesssion():
-    coord_approx = coord_altaz.transform_to(ICRS)
+    with no_polar_motion():
+        t0 = perf_counter()
+        coord_approx = coord_altaz.transform_to(ICRS)
+        approx_time = perf_counter() - t0
+        print(f'Approximate transforms took:   {approx_time:.4} s')
+        print(f'which is {astropy_time / approx_time:.2f} times faster')
 
 separation = coord_approx.separation(coord).arcsecond
 dra = (coord_approx.ra - coord.ra).wrap_at('180d').arcsecond
